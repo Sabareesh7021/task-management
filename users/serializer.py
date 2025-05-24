d39b84a05eb436a887e5e0a4ce4a59acf38417e9
Sabareesh S <sabareeshs@Sabareeshs-Air.lan>
2025-05-24 15:02:42 +0530

    user adding fixed

--- users/serializer.py
+++ users/serializer.py
@@ -32,15 +32,19 @@ class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
     
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
 
     def update(self, instance, validated_data):
